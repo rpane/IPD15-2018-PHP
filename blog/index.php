44975677e98b46bf268279d162cd1c65332eb607
Roberto Panetta <0639300@johnabbottcollege.net>
2019-01-09 11:56:30 -0500

    ..

--- blog/index.php
+++ blog/index.php
@@ -1,9 +1,7 @@
 <?php
 session_start();
 require_once 'db.php';
-?>
-
-<!DOCTYPE html>
+?><!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
@@ -15,23 +13,27 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <h1>Welcome to my blog, read on!</h1>
         <?php
-        $query = "SELECT a.id, a.creationTime, a.title, a.body, u.username FROM articles as a , users as u WHERE a.authorId = u.id"; //WHERE ='%s'", mysqli_real_escape_string($link, $userid));
+        $query = "SELECT a.id, a.creationTime, a.title, a.body, u.username authorName " .
+                " FROM articles as a, users as u WHERE a.authorId = u.id";
         $result = mysqli_query($link, $query);
         if (!$result) {
-            echo "<p>Error: SQL Database query error: " . mysqli_errno($link) . "</p>";
+            echo "<p>Error: SQL database query error: " . mysqli_error($link) . "</p>";
             exit;
-        }        
-        echo "<div id= articlesList>\n";
+        }
+        
+        echo "<div id=articlesList>\n";
         while ($row = mysqli_fetch_assoc($result)) {
             $id = $row['id'];
             $creationTime = $row['creationTime'];
             $title = $row['title'];
             $body = $row['body'];
-            $authorName = $row['username'];
-            printf("<div><a href=article.php");
-        }        
+            $authorName = $row['authorName'];
+            // print_r($row); echo "<br>\n";
+            printf("<div><a href=article.php?id=%s><b>%s</b></a><br>\nPosted by %s on %s<br>\n%s",
+                    $id, $title, $authorName, $creationTime, $body);
+        }
+        echo "</div>\n";
         ?>
     </body>
 </html>
