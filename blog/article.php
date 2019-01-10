106cabf0be3b9576e7c66aa9e3427231a14192cb
Roberto Panetta <0639300@johnabbottcollege.net>
2019-01-10 09:02:43 -0500

    fixed Articles

--- blog/article.php
+++ blog/article.php
@@ -13,8 +13,8 @@ require_once 'db.php';
         <div id="centeredContent">
             <?php
             $id = $_GET['id'];
-             $query = sprintf("SELECT a.id, a.creationTime, a.title, a.body, u.username authorName " .
-                " FROM articles as a JOIN users as u ON a.authorId = u.id WHERE u.id = '%s'",  mysqli_real_escape_string($link, $id));
+             $query = sprintf("SELECT * " .
+                " FROM articles JOIN users ON articles.authorId = users.id WHERE articles.id = '%s'",  mysqli_real_escape_string($link, $id));
         $result = mysqli_query($link, $query);
         if (!$result) {
             echo "<p>Error: SQL database query error: " . mysqli_error($link) . "</p>";
@@ -27,7 +27,7 @@ require_once 'db.php';
             $creationTime = $row['creationTime'];
             $title = $row['title'];
             $body = $row['body'];
-            $authorName = $row['authorName'];
+            $authorName = $row['username'];
             // print_r($row); echo "<br>\n";
             printf("<div><h1>%s</h1></a><br>\nPosted by %s on %s<br>\n%s",
                      $title, $authorName, $creationTime, $body);
